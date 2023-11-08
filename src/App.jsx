bdee0697f602ecde4bac8b20c54e6310686ab97a
NIX <lsc58461@naver.com>
2023-11-08 14:32:11 +0900

    feat(App): header, HeaderService 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
+import HeaderService from "./components/HeaderService/HeaderService";
 
 const App = () => {
   return (
     <>
       <Header>{/* 버튼 컴포넌트 받아야함 */}</Header>
+      <HeaderService
+        recipientId="20"
+        recipientName={"테스트 입니다."}
+        messageCount={20}
+        profileImageURLs={["example/profileImage01.png", "example/profileImage02.png", "example/profileImage03.png"]}
+      />
       <Outlet />
     </>
   );
