8964319cea14cf54536f1d34deee5af9a62e15f2
Gyuho Lee <j980303@naver.com>
2023-11-08 22:40:59 +0900

    Feat(Option.jsx): Option 컴포넌트 구현

---
+++ src/components/Option/Background.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import styles from './Background.module.css';
+
+const colors = ['beige', 'blue', 'purple', 'green'];
+
+const Background = () => {
+  const [selectedColor, setSelectedColor] = useState('beige');
+
+  const handleClickColor = (color) => {
+    setSelectedColor(color);
+  };
+
+  return (
+    <div className={styles.container}>
+      {colors.map((color) => (
+        <div
+          key={color}
+          onClick={() => handleClickColor(color)}
+          className={`${styles.colorBox} ${styles[color]}`}
+        >
+          {selectedColor === color && <p>Selected</p>}
+        </div>
+      ))}
+      <p>Selected color: {selectedColor}</p>
+    </div>
+  );
+};
+
+export default Background;
