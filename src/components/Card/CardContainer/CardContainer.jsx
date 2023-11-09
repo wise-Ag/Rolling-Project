d9d1de22c1fc3f8c39ddbcc30e3e0ceaf420ed2e
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-10 07:08:56 +0900

    feat(CardContainer.jsx): 버튼클릭시 post/id/message로 이동

--- src/components/Card/CardContainer/CardContainer.jsx
+++ src/components/Card/CardContainer/CardContainer.jsx
@@ -2,14 +2,17 @@ import { useGetMessage } from "../../../data-access/useGetMessage";
 import CardBody from "../CardBody/CardBody";
 import Card from "../Card";
 import CardButtonImage from "../CardImage/CardButtonImage";
+import { useParams } from "react-router-dom";
 
 const CardContainer = () => {
   const { data } = useGetMessage();
+  const { id } = useParams();
 
   return (
     <>
       <Card>
-        <CardButtonImage />
+        {/* plus Button */}
+        <CardButtonImage id={id} />
       </Card>
       {data?.map((item) => {
         return <CardBody key={item.id} item={item} />;
--- src/components/Card/CardImage/CardButtonImage.jsx
+++ src/components/Card/CardImage/CardButtonImage.jsx
@@ -1,10 +1,13 @@
 import style from "./CardButtonImage.module.css";
 import plusbutton from "../../../assets/images/icons/plusbutton.svg";
+import { Link } from "react-router-dom";
 
-const CardButtonImage = () => {
+const CardButtonImage = ({ onClick, id }) => {
   return (
     <div className={style.container}>
-      <img src={plusbutton} alt="plus-button" />
+      <Link to={`/post/${id}/message`}>
+        <img src={plusbutton} alt="plus-button" onClick={onClick} />
+      </Link>
     </div>
   );
 };
