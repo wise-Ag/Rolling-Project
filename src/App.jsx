606ac8d09a413e614a9455fdd0e8fc4bcef45885
lsc58461 <lsc58461@naver.com>
2023-11-10 14:11:22 +0900

    refactor: Button 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
 import HeaderService from "./components/HeaderService/HeaderService";
+import Button from "./components/Button/Button";
 
 const App = () => {
   return (
     <>
-      <Header button={<button>테스트 버튼</button> /* 버튼 컴포넌트 받아야함 */} />
+      <Header
+        button={
+          <Button color={"outlined"} size={40}>
+            롤링 페이퍼 만들기
+          </Button>
+        }
+      />
       <HeaderService
         recipientId="20"
         recipientName={"테스트 입니다."}
