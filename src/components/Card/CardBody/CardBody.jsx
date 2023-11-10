8efbb8ddf0c0fe4a37579e45216ac3287d0e09f3
손지은 <wiseag@naver.com>
2023-11-10 15:27:50 +0900

    feat(Card) : Modal기능 추가

--- src/components/Card/CardBody/CardBody.jsx
+++ src/components/Card/CardBody/CardBody.jsx
@@ -3,25 +3,46 @@ import CardProfileImage from "../CardImage/CardProfileImage";
 import CardProfile from "../CardProfile/CardProfile";
 import CardContent from "../CardContent/CardContent";
 import CardDate from "../CardDate/CardDate";
+import Modal from "../../Modal/Modal";
+import CardModal from "../../Modal/CardModal";
+import Button from "../../Button/Button";
 import style from "./CardBody.module.css";
 import bin from "../../../assets/images/icons/bin.svg";
 import { useLocation } from "react-router-dom";
+import { useModal } from "../../../hooks/useModal";
 
 const CardBody = ({ item }) => {
   const location = useLocation();
   const isEditPage = location.pathname.endsWith("edit");
   const { profileImageURL, sender, relationship, content, createdAt } = item;
+
+  const { isModalVisible, openModalFunc, closeModalFunc } = useModal();
+  const handleCardClick = () => {
+    openModalFunc();
+  };
   return (
-    <Card>
-      <div className={style.header}>
-        <CardProfileImage profileImageURL={profileImageURL} />
-        <CardProfile sender={sender} relationship={relationship} />
-        {isEditPage && <img className={style.bin} src={bin} alt="bin" />}
+    <>
+      <div className={style.root} onClick={handleCardClick}>
+        <Card>
+          <div className={style.header}>
+            <CardProfileImage profileImageURL={profileImageURL} />
+            <CardProfile sender={sender} relationship={relationship} />
+            {isEditPage && <img className={style.bin} src={bin} alt="bin" />}
+          </div>
+          <div className={style.divider}></div>
+          <CardContent content={content} />
+          <CardDate createdAt={createdAt} />
+        </Card>
       </div>
-      <div className={style.divider}></div>
-      <CardContent content={content} />
-      <CardDate createdAt={createdAt} />
-    </Card>
+      {isModalVisible && (
+        <Modal>
+          <CardModal />
+          <Button onClick={closeModalFunc} width="12" size="40" color="primary">
+            확인
+          </Button>
+        </Modal>
+      )}
+    </>
   );
 };
 
