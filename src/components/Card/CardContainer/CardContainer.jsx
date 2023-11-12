8619a76de1918314b07e94549df8c39b33b1badb
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-12 10:40:57 +0900

    feat: infinityscroll

--- src/components/Card/CardContainer/CardContainer.jsx
+++ src/components/Card/CardContainer/CardContainer.jsx
@@ -5,12 +5,76 @@ import CardButtonImage from "../CardImage/CardButtonImage";
 import { useParams } from "react-router-dom";
 import { useAsync } from "../../../hooks/useAsync";
 import getRecipientMessages from "../../../apis/getRecipientMessages";
+import { useEffect, useState } from "react";
 
 const CardContainer = () => {
+  const LIMIT = 8;
   const { id } = useParams();
+  const [offset, setOffset] = useState(8);
+  const [items, setItems] = useState([]);
+  const [fetching, setFetching] = useState(false);
+  const { data } = useAsync(getRecipientMessages, {
+    recipientId: id,
+    limit: LIMIT,
+    offset: offset,
+  });
 
-  const { data } = useAsync(getRecipientMessages, { recipientId: id });
-  const results = data?.results || [];
+  console.log(data);
+
+  useEffect(() => {
+    // 이 데이터는 items state값을 말하는건가?
+    if (data) {
+      setItems(data.results || []);
+    }
+  }, [data]);
+
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+
+    // fecthing이 false이면은  즉 더 많은 데이터를 안 가지고 오는 상태
+    if (fetching === false && scrollTop + clientHeight >= scrollHeight) {
+      setFetching(true);
+    }
+    // 데이터를 더 가져오는 함수 쓰기
+  };
+
+  const fetchMoreData = async () => {
+    const data = await getRecipientMessages({
+      recipientId: id,
+      limit: LIMIT,
+      offset: offset,
+    });
+
+    const {
+      result: { next, results },
+    } = data;
+
+    if (!next) {
+      return;
+    }
+    // const newResult = results || [];
+    setItems((prev) => [...prev, ...results]);
+    setOffset((prev) => prev + 8);
+    // 데이터를 다 가져왔음 다시 false로
+    setFetching(false);
+  };
+
+  // 더 불러오기가 실행이 됬을때 실행되는 유즈이펙트
+  useEffect(() => {
+    if (fetching && items.length > 0) {
+      fetchMoreData();
+    }
+  }, [fetching, items]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -18,7 +82,7 @@ const CardContainer = () => {
         {/* plus Button */}
         <CardButtonImage id={id} />
       </Card>
-      {results?.map((item) => {
+      {items?.map((item) => {
         return <CardBody key={item.id} item={item} />;
       })}
     </>
