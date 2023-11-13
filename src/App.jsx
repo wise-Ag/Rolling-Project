9c91ff030f7bd0e65d062a87722804e8eca5339e
lsc58461 <lsc58461@naver.com>
2023-11-13 15:30:20 +0900

    refactor(App): Route element 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
+import CardListPage from "./pages/CardListPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="list" element={<Index />} />
+        <Route path="list" element={<CardListPage />} /* 이정윤 */ />
         <Route path="post" element={<Index />} /* 정진호 */ />
         <Route path="post/:id" element={<Index />} />
         <Route path="post/:id/edit" element={<Index />} />
