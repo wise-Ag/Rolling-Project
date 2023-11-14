4ca6a8ac0c6abce7a0a1e7ff92215abf015ebbe7
손지은 <wiseag@naver.com>
2023-11-14 15:57:07 +0900

    style(LandingPage) : css 수정

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,8 @@
 import Header from "../components/Header/Header";
 import Button from "../components/Button/Button";
 import style from "./LandingPage.module.css";
-import frame from "../assets/images/etc/frame.png";
 import point1 from "../assets/images/etc/point1.svg";
 import point2 from "../assets/images/etc/point2.svg";
-import frame2 from "../assets/images/etc/frame2.png";
 
 import { useNavigate } from "react-router-dom";
 const LandingPage = () => {
@@ -28,22 +26,24 @@ const LandingPage = () => {
                 롤링 페이퍼를 만들 수 있어요
               </p>
             </div>
-            <p className={style.description}>로그인없이자유롭게만들어요</p>
+            <p className={style.description}>로그인 없이 자유롭게 만들어요</p>
           </div>
-          <img src={frame} alt="frame1" className={style.image} />
+          <div className={style.frameImage1}></div>
         </div>
         <div className={style.box2}>
-          <img src={frame2} alt="frame2" className={style.image} />
+          <div className={style.frameImage2}></div>
           <div className={style.frame}>
             <img src={point2} alt="point2" />
-            <span className={style.title}>
+            <div className={style.title}>
               <p>
                 서로에게 이모지로 감정을
                 <br className={style.linebreak} />
                 표현해보세요
               </p>
-            </span>
-            <p>롤링 페이퍼에 이모지를 추가할 수 있어요</p>
+            </div>
+            <p className={style.description}>
+              롤링 페이퍼에 이모지를 추가할 수 있어요
+            </p>
           </div>
         </div>
         <Button
