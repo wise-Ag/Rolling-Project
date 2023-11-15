32f399a15269d9adac1b4fb4236f5daa3a0fdee0
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-15 20:01:47 +0900

    feat:공유이미지변경

--- src/hooks/useKaKao.jsx
+++ src/hooks/useKaKao.jsx
@@ -15,7 +15,7 @@ export const useKaKao = () => {
       templateId: 100601,
       templateArgs: {
         message: `${name}님께 그동안 하지 못했던 속마음을 전해보세요`,
-        THU: "https://cdn.pixabay.com/photo/2017/11/05/08/38/christmas-2919725_640.jpg",
+        THU: "https://media.discordapp.net/attachments/1172446291884769373/1174277548377645096/40d7a939fa3bc82b_.jpg?ex=65670230&is=65548d30&hm=418e93e0934ab3ec319eaec3f396efd5be3d2d92cec12c9be8b7799e992aa715&=&width=549&height=549",
       },
     });
   };
